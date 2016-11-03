202b93771dddc7d1dff88141a4f96e905ce3ab77
Renee <rchar20@gmail.com>
2016-11-03 12:59:23 -1000

    runs function multiple times, unable to get all videos

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,9 +1,33 @@
 angular.module('myApp')
-  .controller( 'myController', ['$scope','PhotoService',
-    ($scope, PhotoService) => {
+  .controller( 'myController', ['$scope','$sce','PhotoService',
+    ($scope, $sce, PhotoService) => {
 
       $scope.photoArr = [];
+      $scope.display = 'grid';
+
+      $scope.trustedUrls = [];
+
       PhotoService.getPhotos().success((photo) => {
         $scope.photoArr = photo.data;
+        photo.data.forEach(img => {
+          if(img.type === 'video') {
+            $scope.trustedUrls.push($sce.trustAsResourceUrl(img.videos.standard_resolution.url));
+            console.log('length',$scope.trustedUrls.length);
+          }
+        })
       })
+
+      $scope.getVidUrl = () => {
+        console.log('getting')
+        return $scope.trustedUrls.shift()
+      }
+
+      $scope.listView = () => {
+          $scope.display = PhotoService.listView();
+      }
+
+      $scope.gridView = () => {
+        $scope.display = PhotoService.gridView();
+      }
+
     }]);
