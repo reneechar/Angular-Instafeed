0084094d5a78e54aad6853a7b39a3975035e81ca
Renee <rchar20@gmail.com>
2016-11-03 16:22:07 -1000

    displays date on list view

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -6,21 +6,27 @@ angular.module('myApp')
       $scope.display = 'grid';
 
       PhotoService.getPhotos().success((photo) => {
-        console.log(photo)
         for (var i = 0; i < photo.data.length; i++) {
           if(photo.data[i].type === 'video') {
             photo.data[i].videos.standard_resolution.url = $sce.trustAsResourceUrl(photo.data[i].videos.standard_resolution.url)
           }
+          var date = new Date(photo.data[i].created_time*1000);
+          var month = date.getUTCMonth();
+          var day = date.getDay();
+          var year = date.getFullYear();
+          var formattedTime = month + '-' + day + '-' + year
+          photo.data[i].created_time = formattedTime
         }
         $scope.photoArr = photo.data;
       })
 
       $scope.listView = () => {
-          $scope.display = PhotoService.listView();
+          $scope.display = 'list';
       }
 
       $scope.gridView = () => {
-        $scope.display = PhotoService.gridView();
+        $scope.display = 'grid';
       }
+
 
     }]);
